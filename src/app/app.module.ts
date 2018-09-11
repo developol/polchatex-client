623ab8a09cd84fa90168d7e945aa360226aa1617
Marek Baraniecki <czapol321@gmail.com>
2018-09-11 16:12:29 +0200

    added navbar and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ChatlistComponent } from './chatlist/chatlist.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatlistComponent,
+    NavbarComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
