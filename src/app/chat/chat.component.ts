7184cf76f48134e826d0eb7c18dcba6aa0a1a14b
Marek Baraniecki <czapol321@gmail.com>
2018-09-12 11:39:43 +0200

    chat view html/css v1.0

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,6 +6,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  messages: any[] = [{
+  "content": "This is some text within a card body.",
+    "class": "sent-message"
+},
+{
+  "content": "This is some text within a card body.",
+  "class": "sent-message"
+},
+{
+  "content": "This is some text within a card body.",
+  "class": "received-message"
+},
+{
+  "content": "This is some text within a card body.",
+  "class": "sent-message"
+},
+{
+  "content": "This",
+  "class": "sent-message"
+},
+{
+  "content": "This xD",
+  "class": "received-message"
+},
+{
+  "content": "This is some text within a card body.",
+  "class": "received-message"
+},
+{
+  "content": "This is some text within a card body. \"class\": background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda;\"sent-message\" \"class\": \"sent-message\" \"class\": \"sent-message\" \"class\": \"sent-message\"This is some text within a card body.",
+  "class": "received-message"
+},
+{
+  "content": "This is some text within a card body. This is some text within a card body. This is some text within a card body. This is some text within a card body. This is some text within a card body.",
+  "class": "sent-message"
+},
+    {
+      "content": "This xD",
+      "class": "received-message"
+    },
+    {
+      "content": "This is some text within a card body.",
+      "class": "received-message"
+    },
+    {
+      "content": "This is some text within a card body. \"class\": background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda;\"sent-message\" \"class\": \"sent-message\" \"class\": \"sent-message\" \"class\": \"sent-message\"This is some text within a card body.",
+      "class": "received-message"
+    },
+    {
+      "content": "This is some text within a card body. This is some text within a card body. This is some text within a card body. This is some text within a card body. This is some text within a card body.",
+      "class": "sent-message"
+    },
+    {
+      "content": "This xD",
+      "class": "received-message"
+    },
+    {
+      "content": "This is some text within a card body.",
+      "class": "received-message"
+    },
+    {
+      "content": "This is some text within a card body. \"class\": background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda; background-color: #ffedda;\"sent-message\" \"class\": \"sent-message\" \"class\": \"sent-message\" \"class\": \"sent-message\"This is some text within a card body.",
+      "class": "received-message"
+    },
+    {
+      "content": "This is some text within a card body. This is some text within a card body. This is some text within a card body. This is some text within a card body. This is some text within a card body.",
+      "class": "sent-message"
+    }
+];
 
   constructor() { }
 
