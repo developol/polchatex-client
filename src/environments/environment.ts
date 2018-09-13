e92185e8f19c30a542a9237ac6504e342b6e9a26
Paweł Kowański <pawel.kowanski@hotmail.com>
2018-09-13 11:27:46 +0200

    * added in environment : commented out urls to heroku server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+  //url: "https://agile-hollows-19556.herokuapp.com",
+  //wsUrl: "wss://agile-hollows-19556.herokuapp.com",
   url: "http://localhost:8080",
   wsUrl: "ws://localhost:8080",
   webSocketEndpoint: "/socket",
