7dc9b8755b56dbcacebdf85b782bb577e1fc1359
Marek Baraniecki <czapol321@gmail.com>
2018-09-13 13:39:49 +0200

    added full authentication service: -token authentication with spring -setting the browser cookie -calling websocketService only when authenticated

---
+++ src/app/shared/service/authentication.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, Subject} from 'rxjs';
+import {CookieService} from 'ngx-cookie-service';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private _credentials: any;
+  private b64Header: HttpHeaders;
+  private jwtToken: string;
+  private readonly tokenCookie: Subject<boolean>;
+
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+    this.tokenCookie = new Subject<boolean>();
+    this._credentials = {
+      "username": 'dupa',
+      "password": 'dupa'
+    }
+    this.b64Header = new HttpHeaders({
+      authorization : 'Basic ' + btoa(this._credentials.username + ':' + this._credentials.password)
+    });
+    this.setCookie();
+  }
+
+
+  basicAuthentication(): Observable<string> {
+    return this.http.get(environment.url + "/rest/tknauth", {headers: this.b64Header, responseType: "text"});
+  }
+
+  setCookie(): void {
+    this.basicAuthentication().subscribe((token: string) => {
+      this.jwtToken = token;
+      console.log(this.jwtToken);
+      this.cookieService.set("JSESSIONID", this.jwtToken, null,"/", "localhost", false);
+      this.setTokenCookie(this.checkIfCookieExists());
+    });
+  }
+
+  checkIfCookieExists():boolean  {
+    return this.cookieService.check('JSESSIONID')
+  }
+
+  getTokenCookieObservable(): Observable<boolean> {
+    return this.tokenCookie.asObservable();
+  }
+  setTokenCookie(token: boolean): void {
+    this.tokenCookie.next(token);
+  }
+
+
+
+  get credentials(): any {
+    return this._credentials;
+  }
+
+  set credentials(value: any) {
+    this._credentials = value;
+  }
+
+}
