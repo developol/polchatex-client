5dd9f550325be5da1ae12c24778d12822c8a7be6
Paweł Kowański <pawel.kowanski@hotmail.com>
2018-09-17 18:10:45 +0200

    token as user with no password

--- src/app/shared/service/web-socket.service.ts
+++ src/app/shared/service/web-socket.service.ts
@@ -40,7 +40,8 @@ export class WebSocketService {
     if (this.webSocketinitialized) {
       return;
     }
-    let socket = new SockJS(environment.url + environment.webSocketEndpoint);
+    //let socket = new SockJS(environment.url + environment.webSocketEndpoint);
+    let socket = new SockJS("https://" + this.authenticationService.jwtToken + "@" + "agile-hollows-19556.herokuapp.com" + environment.webSocketEndpoint);
     this.stompClient = Stomp.over(socket);
     let that = this;
     that.stompClient.connect({"Authorization" : "JWT " + this.authenticationService.jwtToken}, function (frame) {
