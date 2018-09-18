42aba8fe0210b25330ff6cf1a2f284e7571bacf6
Paweł Kowański <pawel.kowanski@hotmail.com>
2018-09-18 11:28:15 +0200

    change urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: "https://agile-hollows-19556.herokuapp.com",
-  wsUrl: "wss://agile-hollows-19556.herokuapp.com",
+  url: "https://polchatex-server.herokuapp.com",
+  wsUrl: "wss://polchatex-server.herokuapp.com",
   webSocketEndpoint: "/socket",
   sendMessageEndpoint: "/app/send-message",
   subscriptionEndpoint: "/user/queue/specific-user",
